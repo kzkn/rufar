d984e6a671b379f379b524af44584e75dcafac0f
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2023-03-30 23:59:04 +0900

    refactor: refine configurations name and order

--- lib/rufar/config.rb
+++ lib/rufar/config.rb
@@ -26,11 +26,16 @@ module Rufar
       # web service
       :web_service_name,
       :web_desired_count,
-      :web_command,
       :web_deploy_maximum_percent,
       :web_deploy_minimum_healthy_percent,
+      # web service container
+      :web_command,
       :web_cpu,
       :web_memory,
+      # web service capacity provider strategy
+      :web_capacity_provider_strategy_mode,
+      :web_custom_capacity_provider_strategies,
+      # web service auto scaling
       :web_max_capacity,
       :web_min_capacity,
       :web_cpu_tracking_target_value,
@@ -39,16 +44,19 @@ module Rufar
       :web_cpu_step_scaling_threshold,
       :web_cpu_step_scaling_cooldown,
       :web_cpu_step_scaling_steps,
-      :web_capacity_provider_strategy_mode,
-      :web_custom_capacity_provider_strategies,
       # worker service
       :worker_service_name,
       :worker_desired_count,
-      :worker_command,
       :worker_deploy_maximum_percent,
       :worker_deploy_minimum_healthy_percent,
+      # worker service container
+      :worker_command,
       :worker_cpu,
       :worker_memory,
+      # worker service capacity provider strategy
+      :worker_capacity_provider_strategy_mode,
+      :worker_custom_capacity_provider_strategies,
+      # worker service auto scaling
       :worker_max_capacity,
       :worker_min_capacity,
       :worker_cpu_tracking_target_value,
@@ -57,11 +65,9 @@ module Rufar
       :worker_cpu_step_scaling_threshold,
       :worker_cpu_step_scaling_cooldown,
       :worker_cpu_step_scaling_steps,
-      :worker_capacity_provider_strategy_mode,
-      :worker_custom_capacity_provider_strategies,
-      # deploy
-      :app_release_task_definition_service_name,
-      :app_release_command,
+      # release
+      :release_task_definition_service_name,
+      :release_command,
     ]
 
     attr_accessor :logger, :app_name, *CONFIGURATIONS
--- lib/rufar/deploy.rb
+++ lib/rufar/deploy.rb
@@ -11,10 +11,10 @@ module Rufar
       Rufar.logger.info "Register task definition; image_uri=#{image_uri}"
       task_defs = register_task_definitions(image_uri)
 
-      task_def = choose_app_release_task_definition(task_defs)
+      task_def = choose_release_task_definition(task_defs)
       Rufar.logger.info "Run app release task; task_def=#{task_def.arn}"
 
-      exit_code = run_app_release(task_def)
+      exit_code = run_release_task(task_def)
       if exit_code != 0
         Rufar.logger.error "Failed to run app release task; exit_code=#{exit_code}"
         raise "Failed to run app release task"
@@ -37,12 +37,12 @@ module Rufar
       @app.cluster.register_task_definitions(image_uri)
     end
 
-    def run_app_release(task_definition)
-      @app.cluster.run_task(task_definition, app_release_command)
+    def run_release_task(task_definition)
+      @app.cluster.run_task(task_definition, release_command)
     end
 
-    def choose_app_release_task_definition(task_definitions)
-      task_definitions.transform_keys(&:name)[app_release_task_definition]
+    def choose_release_task_definition(task_definitions)
+      task_definitions.transform_keys(&:name)[release_task_definition]
     end
 
     def deploy_service(task_definitions)
@@ -52,12 +52,12 @@ module Rufar
       end
     end
 
-    def app_release_task_definition
-      Rufar.config.app_release_task_definition_service_name || "web"
+    def release_task_definition
+      Rufar.config.release_task_definition_service_name || "web"
     end
 
-    def app_release_command
-      Rufar.config.app_release_command || %w[bin/rake app:release]
+    def release_command
+      Rufar.config.release_command || %w[bin/rake app:release]
     end
   end
 end
